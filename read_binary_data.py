7cce7f52386b29449f842efb3034e63d816b3cef
Simon Zimmerman <Simon Zimmerman>
2016-11-15 15:50:18 -0800

    created python function for reading in the box data I generated using darknet and YOLO

---
+++ read_binary_data.py
@@ -0,0 +1,26 @@
+import struct
+def read_binary_file(filename):
+	'''
+	boxes: array of [cx, cy, w, h] (center format)
+    probs: array of probabilities
+	'''
+	f = open(filename)
+	x = f.read(4)
+	classes = []
+	while x:
+		num_boxes = struct.unpack('i',x)
+		boxes = []
+		probs = []
+		for i in range(num_boxes[0]):
+			box = []
+			box.append(struct.unpack('f',f.read(4))[0])
+			box.append(struct.unpack('f',f.read(4))[0])
+			box.append(struct.unpack('f',f.read(4))[0])
+			box.append(struct.unpack('f',f.read(4))[0])
+			prob = struct.unpack('f',f.read(4))
+			boxes.append(box)
+			probs.append(prob)
+		classes.append((boxes, probs))
+		x = f.read(4)
+
+	return classes
