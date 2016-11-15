8998a3251848dd5282e2e66b83d06e77c3c67b5b
Hankun Zhao <jerryz123@berkeley.edu>
2016-11-14 23:47:17 -0800

    Added framework for Python interaction with C

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # nms-speedup
 A highly parallelized implementation of non-maximum suppression for object detection, used primarily for autonomous self-driving cars.
+
+# Installation
+~~~~
+python3 setup.py build install
+~~~~
+
+Run optimized functions with
+~~~~
+python3 -i nms_optimized.py
+~~~~
+
---
+++ nms_optimized.py
@@ -0,0 +1,62 @@
+from nms import nms_c_src, nms_simd_src, nms_gpu_src # Our methods
+# Include optimized versions of NMS here
+
+# C version of NMS, for benchmarking purposes only
+def nms_c(boxes, prob, threshold, form='center'):
+    
+    assert form == 'center' or form == 'diagonal', \
+        'bounding box format not accepted: {}.'.format(form)
+
+    if form == 'diagonal':
+        # convert to center format
+        boxes = [bbox_transform_inv(b) for b in boxes]
+
+    nms_c_src() # Work should be done in here
+    
+    return
+
+# CPU optimized NMS
+def nms_simd(boxes, prob, threshold, form='center'):
+
+    assert form == 'center' or form == 'diagonal', \
+        'bounding box format not accepted: {}.'.format(form)
+
+    if form == 'diagonal':
+        # convert to center format
+        boxes = [bbox_transform_inv(b) for b in boxes]
+
+    nms_simd_src() # Work should be done in here
+        
+    return
+
+# GPU optimized NMS
+def nms_gpu(boxes, prob, threshold, form='center'):
+
+    assert form == 'center' or form == 'diagonal', \
+        'bounding box format not accepted: {}.'.format(form)
+
+    if form == 'diagonal':
+        # convert to center format
+        boxes = [bbox_transform_inv(b) for b in boxes]
+
+    nms_gpu_src() # Work should be done in here
+        
+    return
+
+
+
+def bbox_transform_inv(bbox):
+  """convert a bbox of form [xmin, ymin, xmax, ymax] to [cx, cy, w, h]. Works
+  for numpy array or list of tensors.
+  """
+  xmin, ymin, xmax, ymax = bbox
+  out_box = [[]]*4
+  
+  width       = xmax - xmin + 1.0
+  height      = ymax - ymin + 1.0
+  out_box[0]  = xmin + 0.5*width 
+  out_box[1]  = ymin + 0.5*height
+  out_box[2]  = width
+  out_box[3]  = height
+
+  return out_box
