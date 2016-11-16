c666e6b2e336092a608b6c500cee915343fd9eb0
Vy-An Phan <vyanphan@gmail.com>
2016-11-15 19:14:36 -0800

    added iou c code

--- nmsModule.c
+++ nmsModule.c
@@ -1,43 +1,40 @@
 #include <stdlib.h>
+#include <math.h>
+/* BICHEN'S ORIGINAL IOU CODE (wrong? negative values when both boxes same)
+*/
+float* iou(float *box1, float *box2) {
+	float lr = min(box1[0]+0.5*box1[2], box2[0]+0.5*box2[2]) - 
+			   max(box1[0]-0.5*box1[2], box2[0]-0.5*box2[2]) + 1.0;
+	if(lr > 0) {
+		float tb = min(box1[1]+0.5*box1[3], box2[1]+0.5*box2[3]) - 
+				   max(box1[1]-0.5*box1[3], box2[1]-0.5*box2[3]) + 1.0;
+	    if(tb > 0) {
+	    	float box_inxct= tb*lr;
+	        float box_union = box1[2]*box1[3] + box2[2]*box2[3] - box_inxct; 
+	        return box_inxct / box_union;
+	    }
+	}
+	return 0;
+}
 
-// def iou(box1, box2):
-//   """Compute the Intersection-Over-Union of two given boxes.
 
-//   Args:
-//     box1: array of 4 elements [cx, cy, width, height].
-//     box2: same as above
-//   Returns:
-//     iou: a float number in range [0, 1]. iou of the two boxes.
-//   """
 
-//   lr = min(box1[0]+0.5*box1[2], box2[0]+0.5*box2[2]) - \
-//       max(box1[0]-0.5*box1[2], box2[0]-0.5*box2[2]) + 1.0
-//   if lr > 0:
-//     tb = min(box1[1]+0.5*box1[3], box2[1]+0.5*box2[3]) - \
-//         max(box1[1]-0.5*box1[3], box2[1]-0.5*box2[3]) + 1.0
-//     if tb > 0:
-//       intersection = tb*lr
-//       union = box1[2]*box1[3]+box2[2]*box2[3]-intersection
-//       return intersection/union
-
-//   return 0
-
-/* Scalar naive implementation of NMS, for benchmarking */
-// nms_simd(boxes, prob, threshold, form='center')
+/* 	Scalar naive implementation of NMS, for benchmarking
+	for i in range(len(order)):
+		if not keep[order[i]]:
+		    continue
+		for j in range(i+1, len(order)):
+		    if iou(boxes[order[i]], boxes[order[j]]) > threshold:
+		        keep[order[j]] = False
+	return keep
+*/
 void* nms_c_src(float *boxes, int *order, int *keep, float threshold, int n) {
-//  for i in range(len(order)):
-// 	    if not keep[order[i]]:
-//          continue
-//      for j in range(i+1, len(order)):
-//          if iou(boxes[order[i]], boxes[order[j]]) > threshold:
-//              keep[order[j]] = False
-//  return keep
   	for(int i=0; i<n; i++) {
   		if(!keep[order[i]]) {
   			continue;
   		}
   		for(int j=i+1; j<n; j++) {
-  			if(iou(boxes[order[i]], boxes[order[j]]) > threshold) {
+  			if(iou(boxes + 4*order[i], boxes + 4*order[j]) > threshold) {
   				keep[order[j]] = 0;
   			}
   		}
@@ -45,13 +42,28 @@ void* nms_c_src(float *boxes, int *order, int *keep, float threshold, int n) {
   	return keep;  
 }
 
-/* Scalar naive implementation of NMS, for benchmarking */
+/* Vectorized implementation of NMS, for benchmarking */
 void* nms_simd_src(float *boxes, int *order, int *keep, float threshold, int n) {
+	for(int i=0; i<n; i++) {
+  		if(!keep[order[i]]) {
+  			continue;
+  		}
+  		float boi = boxes[order[i]];
+  		for(int j=i+1; j<n; j+=4) {
+  			/* SIMD SOMETHING HERE 
+				_m128f
 
-    return 0;
+  			*/
+
+  			// if(iou(boxes[order[i]], boxes[order[j]]) > threshold) {
+  			// 	keep[order[j]] = 0;
+  			// }
+  		}
+  	}
+  	return keep;
 }
 
-/* Scalar naive implementation of NMS, for benchmarking */
+/* GPU implementation of NMS, for benchmarking */
 void* nms_gpu_src(float *boxes, int *order, int *keep, float threshold, int n) {
 
     return 0;
