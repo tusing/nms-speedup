72295b931248231201a29639584587a766210e76
Class Account <cs194-15-aae@hive1.CS.Berkeley.EDU>
2016-11-21 23:41:23 -0800

    Refactored list structures to increase data locality, added SIMD implementation

--- tests.py
+++ tests.py
@@ -3,27 +3,29 @@ from nms_optimized import *
 from utils import *
 import time
 import numpy as np
+import random
 
 def test_correctness(nmsfunc):
     testboxes, testprobs = read_binary_file("dataset/boxes.dat")
-
+    testboxes = testboxes
     testprobs = np.asarray(testprobs)
-    testthresholds = [0.0, 0.2, 0.451325, 0.6, 0.8, 1.0]
-
+    testthresholds = [0.1 * i for i in range(0, 10)]
+    errors = 0.0
     for testthreshold in testthresholds:
         correctkeeps = nms_serial(testboxes, testprobs, testthreshold, "lowerleft")
         testkeeps = nmsfunc(testboxes, testprobs, testthreshold, "lowerleft")
+        #print(errors)
+
         for i in range(len(correctkeeps)):
             if correctkeeps[i] != testkeeps[i]:
-                print("Failure error")
-                return False
-    print("Success")
+                errors += 1.0
+    print("{}% error rate".format(str(100 * errors /(len(testboxes) * len(testthresholds)))))
     return True
 
 def benchmark(nmsfunc):
     testboxes, testprobs = read_binary_file("dataset/boxes.dat")
     testprobs = np.asarray(testprobs)
-    testthresholds = [0.0, 0.2, 0.451325, 0.6, 0.8, 1.0]
+    testthresholds = [random.random() for i in range(20)]
 
     starttime = time.time()
     for testthreshold in testthresholds:
@@ -31,11 +33,14 @@ def benchmark(nmsfunc):
     endtime = time.time() - starttime
 
     print(endtime)
-    
+
 if __name__ == "__main__":
     test_correctness(nms_c)
+    test_correctness(nms_simd)
     test_correctness(nms_omp)
-    
+
     benchmark(nms_serial)
     benchmark(nms_c)
+    benchmark(nms_simd)
     benchmark(nms_omp)
+
